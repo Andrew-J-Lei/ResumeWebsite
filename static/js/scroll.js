8d1d2633154be8a64200daa386cd0ac9d37a422b
Andrew-J-Lei <andrew.j.lei.31@gmail.com>
2021-05-04 13:25:49 -0700

    Moving to laptop

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,4 +1,5 @@
+document.documentElement.dataset.fixed = false;
 document.addEventListener("scroll", () => {
   document.documentElement.dataset.scroll = window.scrollY;
-  document.documentElement.dataset.fixed = (window.scrollY > 100);
+  document.documentElement.dataset.fixed = (window.scrollY > 0);
 });
