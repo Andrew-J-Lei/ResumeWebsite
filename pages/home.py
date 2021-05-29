1d7fee60e10b9d96fc2075a4f58264731cd54de5
Andrew-J-Lei <andrew.j.lei.31@gmail.com>
2021-05-28 17:25:24 -0700

    Pre tahoe commit

--- pages/home.py
+++ pages/home.py
@@ -5,9 +5,30 @@ content = """
         <div id="intro_img_wrap">
             <img id="prof_pic" src="https://andrewleiwebresources.blob.core.windows.net/website-resources/1565203348035.jfif" alt="Andrew Lei">
         </div>
-        <div id="waves_wrap">
-            <canvas id="waves">Waves here</canvas>
+    </div>
+    <div class="credentials content_section center">
+        <h1 class="content_heading"> Formal Credentials </h1>
+        <div id="UCSC_logo_wrap">
+            <img id="UCSC_logo" src="https://andrewleiwebresources.blob.core.windows.net/website-resources/UCSC-Logo.png" alt="UCSC Logo">
         </div>
+        <div id="creds_text_fields">
+            <p>
+                Alma Mater: <br />
+                Time of Graduation: <br />
+                Formal Experiences: <br />
+            </p>
+        </div>
+        <div id="creds_text_answers_wrapper">
+            <p>
+                UC Santa Cruz <br />
+                Summer 2020 <br />
+                NONE <br />
+            </p>
+        </div>
+    </div>
+    <div class="interests content_section center">
+    </div>
+    <div class="philosophies content_section center">
     </div>
 </div>
 """
